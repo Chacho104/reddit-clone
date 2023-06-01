2cd6fba226d69a241bf9eaa4a8fb417416ac9f3e
Churchil Owino <chachowino75@gmail.com>
2023-06-01 15:12:45 +0300

    Use firebase auth in cmp

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,8 +2,11 @@ import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import SearchInput from "./SearchInput";
 import RightContent from "./RightContent/RightContent";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
 
 const Navbar: React.FC = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
       <Flex align="center">
@@ -15,7 +18,7 @@ const Navbar: React.FC = () => {
         />
       </Flex>
       <SearchInput />
-      <RightContent />
+      <RightContent user={user} />
       {/* <Directory /> */}
     </Flex>
   );
