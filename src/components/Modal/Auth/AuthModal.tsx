8cdac34a4c232e820ef280eac005065454a6098d
Churchil Owino <chachowino75@gmail.com>
2023-06-01 15:14:36 +0300

    Use reset password cmp in auth modal

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -10,13 +10,17 @@ import {
   Flex,
   Text,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import AuthInputs from "./AuthInputs";
 import OAuthButtons from "./OAuthButtons";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
+import ResetPassword from "./ResetPassword";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+  const [user, loading, error] = useAuthState(auth);
 
   const handleClose = () => {
     setModalState((prev) => ({
@@ -24,6 +28,12 @@ const AuthModal: React.FC = () => {
       open: false,
     }));
   };
+
+  useEffect(() => {
+    if (user) handleClose();
+    console.log("user", user);
+  }, [user]);
+
   return (
     <>
       <Modal isOpen={modalState.open} onClose={handleClose}>
@@ -48,27 +58,32 @@ const AuthModal: React.FC = () => {
               justify="center"
               width="70%"
             >
-              <OAuthButtons />
-              <Flex
-                width="100%"
-                justify="center"
-                borderBottom="1px solid"
-                borderBottomColor="gray.200"
-                m="18px 0"
-              >
-                <Text
-                  color="gray.500"
-                  fontWeight={700}
-                  mb="-12px"
-                  backgroundColor="white"
-                  pr="10px"
-                  pl="10px"
-                >
-                  OR
-                </Text>
-              </Flex>
-              <AuthInputs />
-              {/* <ResetPassword /> */}
+              {modalState.view === "login" || modalState.view === "signup" ? (
+                <>
+                  <OAuthButtons />
+                  <Flex
+                    width="100%"
+                    justify="center"
+                    borderBottom="1px solid"
+                    borderBottomColor="gray.200"
+                    m="18px 0"
+                  >
+                    <Text
+                      color="gray.500"
+                      fontWeight={700}
+                      mb="-12px"
+                      backgroundColor="white"
+                      pr="10px"
+                      pl="10px"
+                    >
+                      OR
+                    </Text>
+                  </Flex>
+                  <AuthInputs />
+                </>
+              ) : (
+                <ResetPassword />
+              )}
             </Flex>
           </ModalBody>
         </ModalContent>
