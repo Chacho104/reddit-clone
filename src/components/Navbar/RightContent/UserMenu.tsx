4ee480ae66ee946dc1c9bf9aa946a46f7ef93329
Churchil Owino <chachowino75@gmail.com>
2023-06-06 21:48:17 +0300

    Reset recoil state on log out

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -17,9 +17,10 @@ import { User, signOut } from "firebase/auth";
 import React from "react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { auth } from "@/src/firebase/clientApp";
-import { useSetRecoilState } from "recoil";
+import { useResetRecoilState, useSetRecoilState } from "recoil";
 import { authModalState } from "@/src/atoms/authModalAtom";
 import { IoSparkles } from "react-icons/io5";
+import { communityState } from "@/src/atoms/communitiesAtom";
 
 type UserMenuProps = {
   user?: User | null;
@@ -27,6 +28,13 @@ type UserMenuProps = {
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
   const setAuthModalState = useSetRecoilState(authModalState);
+  const resetCommunityState = useResetRecoilState(communityState);
+
+  const logout = async () => {
+    await signOut(auth);
+    // clear community state
+    resetCommunityState();
+  };
   return (
     <Menu>
       <MenuButton
@@ -86,7 +94,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
               fontSize="10pt"
               fontWeight={700}
               _hover={{ bg: "blue.500", color: "white" }}
-              onClick={() => signOut(auth)}
+              onClick={logout}
             >
               <Flex align="center">
                 <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
