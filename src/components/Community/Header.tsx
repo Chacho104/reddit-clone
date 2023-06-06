457043704b16fef1c69605a7c803bf294e828b63
Churchil Owino <chachowino75@gmail.com>
2023-06-06 12:55:04 +0300

    Create the community page header component

---
+++ src/components/Community/Header.tsx
@@ -0,0 +1,55 @@
+import { Community } from "@/src/atoms/communitiesAtom";
+import { Box, Button, Flex, Icon, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { FaReddit } from "react-icons/fa";
+
+type HeaderProps = {
+  communityData: Community;
+};
+
+const Header: React.FC<HeaderProps> = ({ communityData }) => {
+  const isJoined = false; // eventually read from communitySnippets
+
+  return (
+    <Flex direction="column" width="100%" height="140px">
+      <Box height="50%" bg="blue.400" />
+      <Flex justify="center" bg="white" flexGrow={1}>
+        <Flex width="95%" maxWidth="860px">
+          {communityData.imageURL ? (
+            <Image />
+          ) : (
+            <Icon
+              as={FaReddit}
+              fontSize={64}
+              position="relative"
+              top={-4}
+              color="blue.500"
+              border="4px solid white"
+              borderRadius="50%"
+            />
+          )}
+          <Flex padding="10px 16px">
+            <Flex direction="column" mr={6}>
+              <Text fontWeight={800} fontSize="16pt">
+                {communityData.id}
+              </Text>
+              <Text fontWeight={600} fontSize="10pt" color="gray.400">
+                r/{communityData.id}
+              </Text>
+            </Flex>
+            <Button
+              variant={isJoined ? "outline" : "solid"}
+              height="30px"
+              pr={6}
+              pl={6}
+              onClick={() => {}}
+            >
+              {isJoined ? "Joined" : "Join"}
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+export default Header;
