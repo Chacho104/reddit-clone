b4afb1fd4f694ccace31c8bf3e9e5bcca8e2baf2
Churchil Owino <chachowino75@gmail.com>
2023-06-09 16:23:04 +0300

    Create a usePosts custom hook

---
+++ src/hooks/usePosts.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { Post, postState } from "../atoms/postsAtom";
+import { firestore, storage } from "../firebase/clientApp";
+import { deleteObject, ref } from "firebase/storage";
+import { deleteDoc, doc } from "firebase/firestore";
+
+const usePosts = () => {
+  const [postStateValue, setPostStateValue] = useRecoilState(postState);
+
+  const onVote = async () => {};
+
+  const onSelectPost = () => {};
+
+  const onDeletePost = async (post: Post): Promise<boolean> => {
+    try {
+      // check if post has an image, delete it from storage
+      if (post.imageURL) {
+        const imageRef = ref(storage, `posts/${post.id}/image`);
+        await deleteObject(imageRef);
+      }
+      // delete post doc from firestore
+      const postDocRef = doc(firestore, "posts", post.id!);
+      await deleteDoc(postDocRef);
+
+      // update recoil state
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: prev.posts.filter((item) => item.id !== post.id),
+      }));
+    } catch (error) {
+      return false;
+    }
+    return true;
+  };
+  return {
+    postStateValue,
+    setPostStateValue,
+    onVote,
+    onSelectPost,
+    onDeletePost,
+  };
+};
+export default usePosts;
