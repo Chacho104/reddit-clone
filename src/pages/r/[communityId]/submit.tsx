1e6e874313167ad046fd719622c8d6c76b23bd62
Churchil Owino <chachowino75@gmail.com>
2023-06-09 16:22:14 +0300

    Use auth state to render new post form cmp

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,16 +1,23 @@
+import { communityState } from "@/src/atoms/communitiesAtom";
 import PageContent from "@/src/components/Layout/PageContent";
 import NewPostForm from "@/src/components/Posts/NewPostForm";
+import { auth } from "@/src/firebase/clientApp";
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilValue } from "recoil";
 
 const SubmitPostPage: React.FC = () => {
+  const [user] = useAuthState(auth);
+  const communityStateValue = useRecoilValue(communityState);
+  console.log("Community", communityStateValue);
   return (
     <PageContent>
       <>
         <Box p="14px 0" borderBottom="1px solid" borderColor="white">
           <Text>Create a post</Text>
         </Box>
-        <NewPostForm />
+        {user && <NewPostForm user={user} />}
       </>
       <>{/* <About /> */}</>
     </PageContent>
