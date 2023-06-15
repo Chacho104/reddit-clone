fe28a55f84e8c5299b6a56133f8e4948f658d0da
Churchil Owino <chachowino75@gmail.com>
2023-06-15 22:11:59 +0300

    Add communityImageURL to new post form

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -20,7 +20,12 @@ const SubmitPostPage: React.FC = () => {
         <Box p="14px 0" borderBottom="1px solid" borderColor="white">
           <Text>Create a post</Text>
         </Box>
-        {user && <NewPostForm user={user} />}
+        {user && (
+          <NewPostForm
+            user={user}
+            communityImageURL={communityStateValue.currentCommunity?.imageURL}
+          />
+        )}
       </>
       <>
         {communityStateValue.currentCommunity && (
