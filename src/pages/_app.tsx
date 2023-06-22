1efe0edfc72c0118fed570cdff9e1f19806913ff
Churchil Owino <chachowino75@gmail.com>
2023-06-22 21:04:26 +0300

    Fix build error finding layout module

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../chakra/theme";
-import Layout from "../components/Layout/Layout";
 import { RecoilRoot } from "recoil";
+import Layout from "../components/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
